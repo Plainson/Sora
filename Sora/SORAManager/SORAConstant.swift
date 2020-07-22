93f35f6ae7d79877a801f8501352ba91bb9389f3
Grass Plainson <PlainsonLGrass@outlook.com>
2020-07-22 19:46:48 +0800

    添加 iCloud 文件路径常数。

---
+++ Sora/SORAManager/SORAConstant.swift
@@ -0,0 +1,13 @@
+//
+//  SORAConstant.swift
+//  Sora
+//
+//  Created by Grass Plainson on 2020/7/22.
+//  Copyright © 2020 Grass Plainson. All rights reserved.
+//
+
+import Foundation
+
+class SORAConstant {
+    static var iCloudUrl: URL?
+}
