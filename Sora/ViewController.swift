87fcb5f509b77063c9c2c7d1aeca97b5f3a13575
Grass Plainson <PlainsonLGrass@outlook.com>
2020-07-22 19:31:01 +0800

    初始化。

---
+++ Sora/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sora
+//
+//  Created by Grass Plainson on 2020/5/9.
+//  Copyright © 2020 Grass Plainson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
