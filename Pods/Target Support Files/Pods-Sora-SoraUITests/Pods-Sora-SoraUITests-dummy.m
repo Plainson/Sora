a9e2cd82b1ef80ec12fd7c34d1d29f8de6a10007
Grass Plainson <PlainsonLGrass@outlook.com>
2020-07-22 19:26:09 +0800

    pod 更新。

---
+++ Pods/Target Support Files/Pods-Sora-SoraUITests/Pods-Sora-SoraUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Sora_SoraUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Sora_SoraUITests
+@end
---
+++ Pods/Target Support Files/Pods-Sora/Pods-Sora-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Sora : NSObject
+@end
+@implementation PodsDummy_Pods_Sora
+@end
