d54c2a7a762a6ed5964c75edb3ddc6fd6362aae4
WangYi <rd331@netvox.com.cn>
2020-07-29 15:58:02 +0800

    增加文件显示视图的一些常量设置。

--- Sora/SORAManager/SORAConstant.swift
+++ Sora/SORAManager/SORAConstant.swift
@@ -7,7 +7,34 @@
 //
 
 import Foundation
+import UIKit
 
 class SORAConstant {
+    
     static var iCloudUrl: URL?
+    
+    static var iCloudDocumentUrl: URL? {
+        if let iCloudUrl = self.iCloudUrl {
+            return iCloudUrl.appendingPathComponent("Documents")
+        } else {
+            return nil
+        }
+    }
+    
+    static var thumbSize: CGSize = CGSize.init(width: SORAConstant.ImageDisplayView.itemSizeWidth * 2, height: SORAConstant.ImageDisplayView.itemSizeHeight * 2)
+    
+    struct ImageDisplayView {
+        static let itemSizeWidth: CGFloat = 90
+        static let itemSizeImageHeight: CGFloat = 70
+        static let itemSizeLabelHeight: CGFloat = 20
+        
+        static let itemSizeEdgeForLabelAndImage: CGFloat = 3
+        static let itemSizeHeight: CGFloat = SORAConstant.ImageDisplayView.itemSizeImageHeight + SORAConstant.ImageDisplayView.itemSizeLabelHeight + SORAConstant.ImageDisplayView.itemSizeEdgeForLabelAndImage
+        
+        static var minLineSpace: CGFloat = 20
+        static let itemTopEdge: CGFloat = 10
+        static let itemLeftEdge: CGFloat = 20
+        static let itemRightEdge: CGFloat = 20
+        static let itemBottomEdge: CGFloat = 10
+    }
 }
