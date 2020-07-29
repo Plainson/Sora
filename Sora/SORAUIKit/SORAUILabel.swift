a1f98b08fa132245c256835c054fcce1216eb88f
WangYi <rd331@netvox.com.cn>
2020-07-29 15:59:44 +0800

    重写 UILabel。

---
+++ Sora/SORAUIKit/SORAUILabel.swift
@@ -0,0 +1,13 @@
+//
+//  SORAUILabel.swift
+//  Sora
+//
+//  Created by Grass Plainson on 2020/7/28.
+//  Copyright © 2020 Grass Plainson. All rights reserved.
+//
+
+import UIKit
+
+class SORAUILabel: UILabel {
+
+}
