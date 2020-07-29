927ddba8149df87e1dbb9ea6a63d5c39a0a57a89
WangYi <rd331@netvox.com.cn>
2020-07-29 15:59:27 +0800

    重写 UIFont。

---
+++ Sora/SORAUIKit/SORAUIFont.swift
@@ -0,0 +1,14 @@
+//
+//  SORAUIFont.swift
+//  Sora
+//
+//  Created by Grass Plainson on 2020/7/28.
+//  Copyright © 2020 Grass Plainson. All rights reserved.
+//
+
+import UIKit
+
+class SORAUIFont: UIFont {
+
+    static let `default`: UIFont = .systemFont(ofSize: 15)
+}
